e54c870ebb66b0abb0bdc38ee1bba7d9e113c23e
Shafeel kalathil <mhd.shfeelkalathil@gmail.com>
2024-11-26 12:29:11 +0530

    initial commit

--- lib/core/error/api_exception.dart
+++ lib/core/error/api_exception.dart
@@ -23,7 +23,32 @@ class ExceptionHandler {
     }
   }
 
+  // String getExceptionMessage(DioException exception) {
+  //   switch (exception.type) {
+  //     case DioExceptionType.connectionTimeout:
+  //     case DioExceptionType.sendTimeout:
+  //     case DioExceptionType.receiveTimeout:
+  //       return 'Connection timed out. Please check your internet connection.';
+  //     case DioExceptionType.badResponse:
+  //       return getErrorMessage(exception.response?.statusCode);
+  //     case DioExceptionType.cancel:
+  //       return 'Request cancelled';
+  //     case DioExceptionType.connectionError:
+  //       return 'No internet connection. Please check your network.';
+  //     case DioExceptionType.badCertificate:
+  //       return 'Something Bad happened';
+  //     case DioExceptionType.unknown:
+  //       if (exception.error is SocketException) {
+  //         return 'No internet connection. Please check your network.';
+  //       }
+  //       return 'An unexpected error occurred.';
+  //   }
+  // }
+
   String getExceptionMessage(DioException exception) {
+    print('DioException: ${exception.toString()}');
+    print('Response: ${exception.response}');
+
     switch (exception.type) {
       case DioExceptionType.connectionTimeout:
       case DioExceptionType.sendTimeout:
@@ -34,14 +59,15 @@ class ExceptionHandler {
       case DioExceptionType.cancel:
         return 'Request cancelled';
       case DioExceptionType.connectionError:
-        return 'No internet connection. Please check your network.';
+        return 'Network error: ${exception.error.toString()}';
       case DioExceptionType.badCertificate:
-        return 'Something Bad happened';
+        return 'SSL certificate issue.';
       case DioExceptionType.unknown:
         if (exception.error is SocketException) {
-          return 'No internet connection. Please check your network.';
+          return 'Socket error: ${exception.error.toString()}';
         }
-        return 'An unexpected error occurred.';
+        return 'An unexpected error occurred: ${exception.error}';
     }
   }
+
 }
